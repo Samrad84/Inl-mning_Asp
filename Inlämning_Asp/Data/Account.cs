8dcd81c4af5a43d35072f29dee8abd83ce467bf5
Alireza Mohammadi <Samrad84@yahoo.com>
2021-04-16 11:21:34 +0200

    Add Account Model

---
+++ Inlämning_Asp/Data/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inlämning_Asp.Data
+{
+    public class Account
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string[] Roles { get; set; }
+    }
+}
---
+++ Inlämning_Asp/Models/AccountModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Inlämning_Asp.Data;
+
+namespace Inlämning_Asp.Models
+{
+    public class AccountModel
+    {
+        private List<Account> accounts;
+
+        public AccountModel()
+        {
+            accounts = new List<Account>() {
+                new Account
+                {
+                    Username = "acc1",
+                    Password = "123",
+                    Roles = new string[]{ "superadmin", "admin", "employee" }
+                },
+                new Account
+                {
+                    Username = "acc2",
+                    Password = "123",
+                    Roles = new string[]{ "admin", "employee" }
+                },
+                new Account
+                {
+                    Username = "acc3",
+                    Password = "123",
+                    Roles = new string[]{ "employee" }
+                }
+            };
+        }
+
+        public Account find(string username)
+        {
+            return accounts.SingleOrDefault(a => a.Username.Equals(username));
+        }
+
+        public Account login(string username, string password)
+        {
+            return accounts.SingleOrDefault(a => a.Username.Equals(username) && a.Password.Equals(password));
+        }
+    }
+
+}
